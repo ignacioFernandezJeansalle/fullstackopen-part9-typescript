5f9d1f08115e2dfb2613156421f25a0d2cce132a
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-10-16 13:14:14 -0300

    Create POST to new patient (ex9.13)

--- patientor/server/src/routes/patientsRouter.ts
+++ patientor/server/src/routes/patientsRouter.ts
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from "express";
 import patientsService from "../services/patientsService";
+import toNewPatient from "../utils";
 
 const router = express.Router();
 
@@ -9,17 +9,17 @@ router.get("/", (_req, res) => {
 });
 
 router.post("/", (req, res) => {
-  const { name, dateOfBirth, ssn, gender, occupation } = req.body;
-
-  const addedEntry = patientsService.addEntry({
-    name,
-    dateOfBirth,
-    ssn,
-    gender,
-    occupation,
-  });
-
-  res.json(addedEntry);
+  try {
+    const newPatient = toNewPatient(req.body);
+    const addedPatient = patientsService.addPatient(newPatient);
+    res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
