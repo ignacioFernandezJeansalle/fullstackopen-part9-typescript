5d41fcfb6bbeb77d643cadd6cb0132b4da9ec27d
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-10-16 10:20:56 -0300

    Add services for diagnoses

---
+++ patientor/server/src/services/diagnosesService.ts
@@ -0,0 +1,17 @@
+import diagnosesData from "../../data/diagnoses";
+import { Diagnose } from "../types";
+
+const diagnoses: Diagnose[] = diagnosesData;
+
+const getEntries = (): Diagnose[] => {
+  return diagnoses;
+};
+
+const addEntry = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addEntry,
+};
