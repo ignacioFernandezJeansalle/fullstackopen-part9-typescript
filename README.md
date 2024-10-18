b4c0f01146d9457734cc233d6e3da5bd16f68581
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-10-18 10:55:52 -0300

    Create App component with data from server (ex9.16)

--- README.md
+++ README.md
@@ -31,7 +31,10 @@ En esta parte, utilizamos las herramientas introducidas anteriormente para crear
 
 - [x] 9.14 => d: React con tipos
 - [x] 9.15 => d: React con tipos
-- [ ] 9.16 => d: React con tipos
+
+[./flight-diary](https://github.com/ignacioFernandezJeansalle/fullstackopen-part9-typescript/tree/main/flight-diary)
+
+- [x] 9.16 => d: React con tipos
 - [ ] 9.17 => d: React con tipos
 - [ ] 9.18 => d: React con tipos
 - [ ] 9.19 => d: React con tipos
--- flight-diary/client/src/App.tsx
+++ flight-diary/client/src/App.tsx
@@ -1,11 +1,72 @@
 import "./App.css";
 
+import { useEffect, useState } from "react";
+import { Diary } from "./types";
+import axios from "axios";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Flight Diary</h1>
+    </header>
+  );
+};
+
+interface DiaryEntries {
+  diaries: Diary[];
+}
+
+const DiaryEntries = (props: DiaryEntries) => {
+  const { diaries } = props;
+
+  return (
+    <section>
+      <h2>Diary entries</h2>
+      <ul>
+        {diaries.map((d) => (
+          <li key={d.id}>
+            <p>
+              <b>{d.date}</b>
+            </p>
+            <p>Visibility: {d.visibility}</p>
+            <p>Weather: {d.weather}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
 function App() {
+  const [diaries, setDiaries] = useState<Diary[]>([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/diaries").then((response) => {
+      const data = response.data as Diary[];
+      setDiaries(data);
+    });
+  }, []);
+
   return (
     <>
-      <h1>Flight Diary</h1>
+      <Header />
+      <main>
+        <DiaryEntries diaries={diaries} />
+      </main>
     </>
   );
 }
 
 export default App;
+
+/* 
+
+{
+  "id": 1,
+  "date": "2017-01-01",
+  "weather": "rainy",
+  "visibility": "poor",
+  "comment": "Pretty scary flight, I'm glad I'm alive"
+ }
+
+*/
