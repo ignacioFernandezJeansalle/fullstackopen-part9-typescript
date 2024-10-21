e109ed0833b26fe50dc0090435b9ec36064910f1
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-10-21 10:06:38 -0300

    Add message to post new entry (ex9.18)

--- flight-diary/client/src/App.tsx
+++ flight-diary/client/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 import { useEffect, useState } from "react";
 import { Diary, newDiary } from "./types";
+import axios from "axios";
 import diaryService from "./services/diaryService";
 
 import Header from "./components/Header";
@@ -10,20 +11,32 @@ import DiaryEntries from "./components/DiaryEntries";
 
 function App() {
   const [diaries, setDiaries] = useState<Diary[]>([]);
+  const [newEntryMessage, setNewEntryMessage] = useState("");
 
   useEffect(() => {
     diaryService.getDiaries().then((data) => setDiaries(data));
   }, []);
 
   const addNewEntry = (object: newDiary) => {
-    diaryService.createDiary(object).then((data) => setDiaries(diaries.concat(data)));
+    diaryService
+      .createDiary(object)
+      .then((data) => setDiaries(diaries.concat(data)))
+      .catch((error: unknown) => {
+        let errorMessage = "Something went wrong...";
+        if (axios.isAxiosError(error)) {
+          errorMessage = error.response?.data;
+        }
+
+        setNewEntryMessage(errorMessage);
+        setTimeout(() => setNewEntryMessage(""), 5000);
+      });
   };
 
   return (
     <>
       <Header />
       <main>
-        <AddNewEntryForm addNewEntry={addNewEntry} />
+        <AddNewEntryForm addNewEntry={addNewEntry} message={newEntryMessage} />
         <DiaryEntries diaries={diaries} />
       </main>
     </>
