f266dffa1a2441d122714e1a79d96f79aa5f9a75
Ignacio Fernandez Jeansalle <ignaciofj@gmail.com>
2024-10-23 13:37:44 -0300

    Update types

--- patientor/client/src/types.ts
+++ patientor/client/src/types.ts
@@ -1,16 +1,57 @@
-export interface Diagnosis {
-  code: string;
-  name: string;
-  latin?: string;
-}
-
 export enum Gender {
   Male = "male",
   Female = "female",
   Other = "other",
 }
 
-export interface Entry {}
+export enum HealthCheckRating {
+  Healthy = 0,
+  LowRisk = 1,
+  HighRisk = 2,
+  CriticalRisk = 3,
+}
+
+export interface Diagnosis {
+  code: string;
+  name: string;
+  latin?: string;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+export type Entry = HospitalEntry | OccupationalHealthCareEntry | HealthCheckEntry;
 
 export interface Patient {
   id: string;
@@ -22,4 +63,19 @@ export interface Patient {
   entries?: Entry[];
 }
 
+/* BACKEND TYPE
+export interface Patient {
+  id: string;
+  name: string;
+  ssn: string;
+  occupation: string;
+  gender: Gender;
+  dateOfBirth: string;
+  entries: Entry[];
+} */
+
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
+
+export type Message = {
+  message: string;
+};
